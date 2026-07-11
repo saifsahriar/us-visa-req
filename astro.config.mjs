2dd72d1c579d1068e37a9de306900f78ead28a1e
saif <saiffsahriar@gmail.com>
2026-07-11 12:26:52 +0530

    fixed trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://usvisarequirements.com',
-  trailingSlash: 'never',
+  trailingSlash: 'always',
   output: 'static',
   adapter: vercel(),
   integrations: [sitemap()],
